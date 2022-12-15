98ff53182b1362f82569f30b0351d71e23a789e3
shakyapeiris <shakyaimanjith32@gmail.com>
2022-12-15 22:46:54 +0530

    Initial Commit

---
+++ calculator.go
@@ -0,0 +1,17 @@
+package calculator
+
+func Add(a float64, b float64) float64 {
+	return a + b
+}
+
+func Substract(a float64, b float64) float64 {
+	return a - b
+}
+
+func Multiply(a float64, b float64) float64 {
+	return a * b
+}
+
+func Divide(a float64, b float64) float64 {
+	return a / b
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shakyapeiris/GO-Calculator
+
+go 1.19
